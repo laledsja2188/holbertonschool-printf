493b036ba15ce5a3c9deb1dc71e7f81955efb43b
laledsja2188 <ceferovalale343@gmail.com>
2025-09-05 17:57:28 +0400

    task

--- _printf.c
+++ _printf.c
@@ -1,65 +1,66 @@
 #include "main.h"
 
 /**
- * _printf - simplified printf implementation
+ * _printf - produces output according to a format
  * @format: format string
+ *
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i = 0, count = 0;
-	char c, *s;
+    va_list args;
+    int i = 0, count = 0;
+    char c;
+    char *str;
 
-	if (!format)
-		return (-1);
+    if (format == NULL)
+        return (-1);
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == 'c')
-			{
-				c = (char) va_arg(args, int);
-				write(1, &c, 1);
-				count++;
-			}
-			else if (format[i] == 's')
-			{
-				s = va_arg(args, char *);
-				if (!s)
-					s = "(null)";
-				while (*s)
-				{
-					write(1, s, 1);
-					s++;
-					count++;
-				}
-			}
-			else if (format[i] == '%')
-			{
-				write(1, "%", 1);
-				count++;
-			}
-			else
-			{
-				write(1, "%", 1);
-				write(1, &format[i], 1);
-				count += 2;
-			}
-		}
-		else
-		{
-			write(1, &format[i], 1);
-			count++;
-		}
-		i++;
-	}
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == 'c') 
+            {
+                c = (char)va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+            }
+            else if (format[i] == 's') 
+            {
+                str = va_arg(args, char *);
+                if (str == NULL)
+                    str = "(null)";
+                while (*str)
+                {
+                    write(1, str, 1);
+                    str++;
+                    count++;
+                }
+            }
+            else if (format[i] == '%')
+            {
+                write(1, "%", 1);
+                count++;
+            }
+            else 
+            {
+                write(1, "%", 1);
+                write(1, &format[i], 1);
+                count += 2;
+            }
+        }
+        else
+        {
+            write(1, &format[i], 1);
+            count++;
+        }
+        i++;
+    }
 
-	va_end(args);
-	return (count);
+    va_end(args);
+    return (count);
 }
-
--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(void)
 {
     _printf("Salam dunya!\n");
     _printf("Bir herf: %c\n", 'A');
-    _printf("Bir soz: %s\n", "ChatGPT");
+    _printf("Bir soz: %s\n", "Hello");
     _printf("Faiz isaresi: %%\n");
     return (0);
 }
